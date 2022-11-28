1df7d3cd48981d2523952a693b68a28a0506fe9b
Febi Ago Jonata <febiago7@gmail.com>
2022-11-28 21:32:05 +0700

    Update: Midleware and Routes

--- middlewares/restrict.js
+++ middlewares/restrict.js
@@ -1,28 +1,69 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
+const { ROLE } = require("../utils/enum");
 
-module.exports = (req, res, next) => {
-  try {
-    const token = req.headers["authorization"];
-    if (!token) {
-      return res.status(401).json({
-        status: false,
-        message: "you're not authorized!",
-        data: null,
-      });
+const {
+    JWT_SIGNATURE_KEY
+} = process.env;
+
+module.exports = {
+    mustLogin: (req, res, next) => {
+        try {
+            const token = req.headers['authorization'];
+            if (!token) {
+                return res.status(401).json({
+                    status: false,
+                    message: 'you\'re not authorized!',
+                    data: null
+                });
+            }
+
+            const decoded = jwt.verify(token, JWT_SIGNATURE_KEY);
+            req.user = decoded;
+
+            next();
+        } catch (err) {
+            if (err.message == 'jwt malformed') {
+                return res.status(401).json({
+                    status: false,
+                    message: err.message,
+                    data: null
+                });
+            }
+
+            next(err);
+        }
+    },
+    mustAdmin: (req, res, next) => {
+    try {
+      const token = req.headers["authorization"];
+      if (!token) {
+        return res.status(401).json({
+          status: false,
+          message: "You're not authorized!",
+          data: null,
+        });
+      }
+
+      const decoded = jwt.verify(token, JWT_SIGNATURE_KEY);
+      req.id = decoded;
+
+      if (decoded.role !== ROLE.ADMIN) {
+        return res.status(403).json({
+          status: false,
+          message: "You're not authorized!, Only admin can access",
+          data: null,
+        });
+      }
+      next();
+    } catch (err) {
+      if (err.message == "Jwt malformed") {
+        return res.status(401).json({
+          status: false,
+          message: err.message,
+          data: null,
+        });
+      }
+      next(err);
     }
-
-    const payload = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    req.user = payload;
-
-    next();
-  } catch (err) {
-    if (err.message == "jwt malformed") {
-      return res.status(401).json({
-        status: false,
-        message: err.message,
-        data: null,
-      });
-    }
-    next(err);
   }
 };
