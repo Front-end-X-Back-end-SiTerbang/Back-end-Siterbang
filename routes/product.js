2e1b55c23da14ad201ef900205864e7647f64091
Deny Septian <17.denyseptian@gmail.com>
2022-12-12 19:28:42 +0700

    Refactor : product search endpoint

--- routes/product.js
+++ routes/product.js
@@ -6,7 +6,7 @@ const mid = require("../middlewares");
 router.get("/all", mid.restrict.mustAdmin, c.product.getAll);
 router.get("/:id", mid.restrict.mustAdmin, c.product.get);
 router.post("/", mid.restrict.mustAdmin, c.product.create);
-router.get("/search", mid.restrict.mustLogin, c.product.search); // tidak perlu login (?)
+router.get("/search", c.product.search);
 router.put("/:id", mid.restrict.mustAdmin, c.product.update);
 router.delete("/:id", mid.restrict.mustAdmin, c.product.delete);
 module.exports = router;
