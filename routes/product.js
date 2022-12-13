d3bc596adee2e0561e6271e7d428448a3c598d6f
Deny Septian <17.denyseptian@gmail.com>
2022-12-13 13:56:07 +0700

    Update : add round trip filter

--- routes/product.js
+++ routes/product.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const c = require("../controllers");
 const mid = require("../middlewares");
 
+router.get("/search", c.product.search);
 router.get("/all", mid.restrict.mustAdmin, c.product.getAll);
 router.get("/:id", mid.restrict.mustAdmin, c.product.get);
 router.post("/", mid.restrict.mustAdmin, c.product.create);
-router.get("/search", mid.restrict.mustLogin, c.product.search); // tidak perlu login (?)
 router.put("/:id", mid.restrict.mustAdmin, c.product.update);
 router.delete("/:id", mid.restrict.mustAdmin, c.product.delete);
 module.exports = router;
