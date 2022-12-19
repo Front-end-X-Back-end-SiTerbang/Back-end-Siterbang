152d7d5f56e3a7c55ead6bd7e84d79cce4285de7
Deny Septian <17.denyseptian@gmail.com>
2022-12-19 21:42:07 +0700

    Test : Refactor user-get-information

--- controllers/user.js
+++ controllers/user.js
@@ -89,7 +89,7 @@ module.exports = {
     const token = req.headers["authorization"];
     const user = jwt.verify(token, JWT_SECRET);
     
-    const userData = await User.findOne({where : {id : user.id} , attributes : ['name', 'email', 'phone' , 'gender', 'postal_code', 'address']})
+    const userData = await User.findOne({where : {id : user.id} , attributes : ['name', 'email', 'phone' , 'gender', 'postal_code', 'address', 'photo']})
     return res.status(200).json({
       status : true,
       message : "success get user information",
